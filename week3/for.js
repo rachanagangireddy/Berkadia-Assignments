96741bcd1e430ea1a2966990d18f865fda448853
Sai Rachana Gangireddy <rachana.raveena@gmail.com>
2018-11-29 23:34:16 +0530

    Add files via upload

---
+++ week3/for.js
@@ -0,0 +1,11 @@
+var cars = [{name:"BMW",price:"70lkh"},{name: "Volvo",price:"80lkh"}];
+var i;
+for (i = 0; i < cars.length; i++) {
+    console.log(cars[i].name,":",cars[i].price);
+}
+/*
+output:=
+PS C:\Users\sai rachana\Desktop\nodejsprograms> node for.js
+BMW : 70lkh
+Volvo : 80lkh
+*/
