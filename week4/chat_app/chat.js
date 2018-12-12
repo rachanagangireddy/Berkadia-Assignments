96f5f9b815f35703dcffc5665b9b1b3e87af1515
Sai Rachana Gangireddy <rachana.raveena@gmail.com>
2018-12-12 19:54:50 +0530

    Create chat.js

---
+++ week4/chat_app/chat.js
@@ -0,0 +1,33 @@
+
+// Make connection ,localhost port num:4000
+var socket = io.connect('http://localhost:4000');
+
+// Query DOM, and getting ids of html page
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
+
+// Emit events when clicking button i.e, send button
+btn.addEventListener('click', function(){
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+    message.value = "";
+});
+
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
+})
+
+// Listen for events
+socket.on('chat', function(data){
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
