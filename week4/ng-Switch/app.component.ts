4f512c20be48adb141e873dfe8cdf0745fce06f4
Sai Rachana Gangireddy <rachana.raveena@gmail.com>
2018-12-18 22:39:15 +0530

    Add files via upload

---
+++ week4/ng-Switch/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NgSwitch } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ // title = 'ngIf and ngFor example';
+ title='NgSwitch';
+  
+public branch='CSE';
+constructor(){}
+ngOnInit(){}
+
+}
