c0ea0515210f62e92e3183141dfb57bb262dd39b
Sai Rachana Gangireddy <rachana.raveena@gmail.com>
2018-12-21 17:11:57 +0530

    Add files via upload

---
+++ week3/Angular/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Interpolation example';
+  my:string='BERKADIA';
+  imgpath:string='http://www.ranchcapital.com/wordpress/wp-content/uploads/2016/05/berkadia-logo.png';
+}
