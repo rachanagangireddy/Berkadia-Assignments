44fda90dbe07a86a87388233a2e2af2f76c12e99
Sai Rachana Gangireddy <rachana.raveena@gmail.com>
2018-12-21 17:29:01 +0530

    Add files via upload

---
+++ week3/angular/propertybinding/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'property binding';
+  my='Berkadia';
+  imgpath='http://www.winewomenandshoes.com/wp-content/uploads/2016/09/Berkadia-Logo.jpeg'
+}
